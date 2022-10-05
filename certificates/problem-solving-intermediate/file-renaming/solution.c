bb0de1649bf88ffe3fe8e39e3607622aa4fdaadf
Mahmoud Nageh <mahmoudnage7@hotmail.com>
2022-10-06 01:07:35 +0400

    c solution

--- certificates/problem-solving-intermediate/file-renaming/solution.c
+++ certificates/problem-solving-intermediate/file-renaming/solution.c
@@ -1,26 +1,27 @@
 #define MOD 1000000007
 
+long temp_arr[1000000];
+long occ_arr[1000000];
+    
 int renameFile(char* newName,char* oldName)
 {
-    int new_lenght = strlen(newName);
-    int old_length = strlen(oldName);
+    long new_lenght = strlen(newName);
+    long old_length = strlen(oldName);
+    
 
-    int temp_arr[10000];
-    int occ_arr[10000];
-
-    for (int z = 0; z < old_length + 1; z++)
+    for (long z = 0; z < old_length + 1; z++)
     {
         temp_arr[z] = 1;
     }
 
-    for (int i = 1; i < new_lenght + 1; i++) 
+    for (long i = 1; i < new_lenght + 1; i++) 
     {
-        for (int z = 0; z < old_length + 1; z++) 
+        for (long z = 0; z < old_length + 1; z++) 
         {
             occ_arr[z] = 0;
         }
 
-        for (int j = i; j < old_length + 1 ; j++)
+        for (long j = i; j < old_length + 1 ; j++)
         {
             occ_arr[j] = occ_arr[j - 1];
 
@@ -29,7 +30,7 @@ int renameFile(char* newName,char* oldName)
 
         }
 
-        for (int z = 0; z < old_length + 1; z++) 
+        for (long z = 0; z < old_length + 1; z++) 
         {
             temp_arr[z] = occ_arr[z];
         }
