f0c0dc0baddc901b1725346b69d81747cd723d78
Mahmoud Nageh <mahmoudnage7@hotmail.com>
2022-10-05 23:08:56 +0400

    c solution

--- certificates/problem-solving-intermediate/largest-area/solution.c
+++ certificates/problem-solving-intermediate/largest-area/solution.c
@@ -5,40 +5,53 @@ int cmp(const void* a, const void* b)
     return *(const int*) a - *(const int*) b;
 }
 
-int maxArea(int w, int h, int* horizontalCuts, int horizontalCutsSize, int* verticalCuts, int verticalCutsSize) 
+int maxH, maxV, boundry;
+
+long maxArea(int w, int h, int* horizontalCuts, int horizontalCutsSize, int* verticalCuts, int verticalCutsSize) 
 {
-    	int maxH = MAX(horizontalCuts[0], h - horizontalCuts[horizontalCutsSize - 1]);
-    	int maxV = MAX(verticalCuts[0], w - verticalCuts[verticalCutsSize - 1]);
+	if ( boundry == 0 )
+	{
+		maxH = MAX(horizontalCuts[0], h - horizontalCuts[horizontalCutsSize - 1]);
+
+		for (int i = 1; i < horizontalCutsSize; i++) 
+		{
+        		maxH = MAX(maxH, horizontalCuts[i] - horizontalCuts[i-1]);
+    		}
+    	}
+	else if(boundry == 1)
+	{
+    		maxV = MAX(verticalCuts[0], w - verticalCuts[verticalCutsSize - 1]);
 	
-    	for (int i = 1; i < horizontalCutsSize; i++) 
-	{
-        	maxH = MAX(maxH, horizontalCuts[i] - horizontalCuts[i-1]);
-    	}
-
-    	for (int i = 1; i < verticalCutsSize; i++) 
-	{
-        	maxV = MAX(maxV, verticalCuts[i] - verticalCuts[i-1]);
-    	}
-
+    		for (int i = 1; i < verticalCutsSize; i++) 
+		{
+        		maxV = MAX(maxV, verticalCuts[i] - verticalCuts[i-1]);
+    		}
+	}
     	return maxH * maxV;
 }
 
-int* getMaxArea(int w, int h, int n, bool* isVertical, int* distance)
+long* getMaxArea(int w, int h, int isVertical_count, bool* isVertical,int distance_count, int* distance, int* result_count)
 {
-	int* result = (int*) malloc(n * sizeof(int));
+	maxH = h;
+	maxV = w;
+
+	*result_count = isVertical_count;
+
+	long* result = (long*) malloc(isVertical_count * sizeof(long));
 
 	// Boundry counters
 	int horizontalCutsSize = 0;
 	int verticalCutsSize   = 0;
 
-	int* horizontalCuts = (int*) malloc(n * sizeof(int));
-	int* verticalCuts   = (int*) malloc(n * sizeof(int));
+	int* horizontalCuts = (int*) malloc(isVertical_count * sizeof(int));
+	int* verticalCuts   = (int*) malloc(isVertical_count,  * sizeof(int));
 
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < isVertical_count, ; i++)
 	{
 		// horizontal Boundry
 		if (isVertical[i] == 0)
 		{
+			boundry = 0;
 			horizontalCuts[horizontalCutsSize] = h - distance[i];
 			horizontalCutsSize++;
 			qsort(horizontalCuts, horizontalCutsSize, sizeof(int), cmp);
@@ -47,16 +60,17 @@ int* getMaxArea(int w, int h, int n, bool* isVertical, int* distance)
 		// Vertical Boundry
 		else if (isVertical[i] == 1)
 		{
+			boundry =1;
 			verticalCuts[verticalCutsSize] = distance[i];
 			verticalCutsSize++;
 			qsort(verticalCuts, verticalCutsSize, sizeof(int), cmp);
 		}
 		
-		result[i] = maxArea(w, h, *horizontalCuts, horizontalCutsSize, *verticalCuts, verticalCutsSize);
+		result[i] = maxArea(w, h, horizontalCuts, horizontalCutsSize, verticalCuts, verticalCutsSize);
 	}
 	
 	free(horizontalCuts);
-	free(verticalCutsSize);
+	free(verticalCuts);
 
 	return result;
 }
