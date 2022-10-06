6226deb11c39e6ff12f2a78a81903f3b1356ff71
Mahmoud Nageh <mahmoudnage7@hotmail.com>
2022-10-06 21:04:11 +0400

    c solution

--- certificates/problem-solving-intermediate/equalizing-array-elements/solution.c
+++ certificates/problem-solving-intermediate/equalizing-array-elements/solution.c
@@ -1,45 +1,37 @@
-int minOp ( *arr, thershold, divisor)
+ int cmp(const void* a, const void* b) 
 {
-	arr_lenght = len(arr);
+    return *(const int*) a - *(const int*) b;
+}
 
-	qsort(arr, arr_length, sizeof(int), cmpfunc);
+int  dp[200001][2] = { 0 };
 
-	int counter = 0;
-	
-	int max = 0;
-	
-	// kind of a hash table for the elements occurrence
-	int *occ_arr = (int*) calloc( arr[arr_lengh -1], sizeof(int));
+int minOperations(int arr_count, int* arr, int threshold, int d) 
+{
+    //sorting
+    
+    qsort(arr, arr_count, sizeof(int), cmp);
+    
+    int minO = INT_MAX;
 
-	
-	for( int i = 0; i < arr_lenght; i++)
-	{
-		occ_arr[arr[i]]++;
-		
-		// check if the thershold is already met..
-		if(occ_arr[arr[i]] == threshold)
-		{
-			return 0;
-		}
-	}
-
-	while (1)
-	{	
-		arr[arr_length -1] = arr[arr_length -1] / 2;
-		
-		occ_arr[arr[arr_length-1]]++;
-
-		counter++;
-
-		if (occ_arr[arr[arr_length-1]] == threshold)
-		{
-			return counter;
-		}
-		
-		else
-		{
-			qsort(arr, arr_length, sizeof(int), cmpfunc);
-		}
-	} 		
-		
+    for (int i = 0; i < arr_count; i++)
+    {
+        int steps = 0;
+        
+        while (1)
+        {
+            int x = arr[i];
+            dp[x][0] += 1;
+            dp[x][1] += steps;
+            
+            if (dp[x][0] >= threshold)
+                minO = ( minO <= dp[x][1] )? minO : dp[x][1];
+                
+            if (x == 0)
+                break;
+                
+            arr[i] /= d;
+            steps += 1;
+        }
+    }
+    return minO;
 }
